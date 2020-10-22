2954bd2f93eb47456aeb603e7841c9d39f56c85b
xpekexoan <yourshapw1989@gmail.com>
2020-10-22 21:30:39 +0700

    Lần 1

---
+++ app/src/main/java/com/example/foody/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.foody;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Fragment_home()).commit();
+        }
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new Fragment_home();
+                            break;
+                        case R.id.nav_saved:
+                            selectedFragment = new Fragment_saved();
+                            break;
+                        case R.id.nav_history:
+                            selectedFragment = new Fragment_history();
+                            break;
+                        case R.id.nav_nofication:
+                            selectedFragment = new Fragment_nofication();
+                            break;
+                        case R.id.nav_user:
+                            selectedFragment = new Fragment_user();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+}
